eff5df109394217accb96db1cd37ab89e9eb7c9d
LE-xcj <865263907@qq.com>
2017-12-31 22:09:37 +0800

    Initial commit

---
+++ zhkuapp/src/main/java/com/example/zhkuapp/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.zhkuapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+    @Bind(R.id.tv)
+    TextView tv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        tv.setText("successful");
+    }
+}
