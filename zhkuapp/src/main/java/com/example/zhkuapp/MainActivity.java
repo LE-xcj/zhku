6d4103727d8d8c2ab4c2dae8e96af2604b46fb83
LE-xcj <865263907@qq.com>
2017-12-31 22:12:21 +0800

    second

--- zhkuapp/src/main/java/com/example/zhkuapp/MainActivity.java
+++ zhkuapp/src/main/java/com/example/zhkuapp/MainActivity.java
@@ -16,7 +16,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-
-        tv.setText("successful");
     }
 }
