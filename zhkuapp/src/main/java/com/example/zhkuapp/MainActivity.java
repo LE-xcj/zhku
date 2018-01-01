44befd2446c1070b105bce7fadd06126b3810154
LE-xcj <865263907@qq.com>
2018-01-01 17:15:08 +0800

    login && regist

--- zhkuapp/src/main/java/com/example/zhkuapp/MainActivity.java
+++ zhkuapp/src/main/java/com/example/zhkuapp/MainActivity.java
@@ -1,23 +1,72 @@
 package com.example.zhkuapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.zhkuapp.view.LoginActivity;
+import com.hyphenate.EMCallBack;
+import com.hyphenate.EMError;
+import com.hyphenate.chat.EMClient;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
-    @Bind(R.id.tv)
-    TextView tv;
+
+
+    // 弹出框
+    private ProgressDialog mDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // 判断sdk是否登录成功过，并没有退出和被踢，否则跳转到登陆界面
+        if (!EMClient.getInstance().isLoggedInBefore()) {
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(intent);
+            finish();
+            return;
+        }
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
-        tv.setText("successful");
-        //修改一下
+    }
+
+    @Override
+    protected void onDestroy() {
+        signOut();
+        super.onDestroy();
+    }
+
+    /**
+     * 退出登录
+     */
+    private void signOut() {
+        // 调用sdk的退出登录方法，第一个参数表示是否解绑推送的token，没有使用推送或者被踢都要传false
+        EMClient.getInstance().logout(false, new EMCallBack() {
+            @Override
+            public void onSuccess() {
+                Log.i("lzan13", "logout success");
+                // 调用退出成功，结束app
+                //finish();
+            }
+
+            @Override
+            public void onError(int i, String s) {
+                Log.i("lzan13", "logout error " + i + " - " + s);
+            }
+
+            @Override
+            public void onProgress(int i, String s) {
+
+            }
+        });
     }
 }
